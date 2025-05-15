1effb38e875659200253867a80a2934494f7fcff
HarryLarke <harrylarke1@hotmail.com>
2025-05-15 13:07:12 +0100

    Server CRUD

--- Backend/routes/api/bundles.ts
+++ Backend/routes/api/bundles.ts
@@ -1,10 +1,15 @@
 import express from 'express'
-import { getAllBundles, createNewBundle } from '../../controllers/bundleController'
+import { getAllBundles, createNewBundle, updateBundle, deleteBundle, getBundle } from '../../controllers/bundleController'
 
 const router = express.Router()
 
 router.route('/')
     .get(getAllBundles)
     .post(createNewBundle)
+    .put(updateBundle)
+    .delete(deleteBundle)
+
+router.route('/:id')
+    .get(getBundle)
 
 export default router
--- Backend/routes/api/cards.ts
+++ Backend/routes/api/cards.ts
@@ -1,7 +1,15 @@
 import express from 'express'
+import { getAllCards, createNewCard, updateCard, deleteCard, getCard  } from '../../controllers/cardController'
 
 const router = express.Router()
 
 router.route('/')
+    .get(getAllCards)
+    .post(createNewCard)
+    .put(updateCard)
+    .delete(deleteCard)
+
+router.route('/:id')
+    .get(getCard)
 
 export default router
